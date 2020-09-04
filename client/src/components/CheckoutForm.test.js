f64cbcd4584c77aa61daaa8bbcb251c4da2cbdd4
git-austinmccollom <austinmccollom@gmail.com>
2020-09-04 11:09:23 -0500

    second test: submit successful

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
-    const wrapper = render(<CheckoutForm />)
-    const header = wrapper.getByText('Checkout Form')
+    const wrapper = render(<CheckoutForm />);
+    const header = wrapper.getByText('Checkout Form');
     expect(header).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {
-
+test("form shows success message on submit with form details", async () => {
+    const wrapper = render(<CheckoutForm />);
+    const submitButton = wrapper.getByText('Checkout');
+    fireEvent.click(submitButton);
+    await waitFor( () => { 
+        expect( wrapper.getByTestId('successMessage') ).toBeInTheDocument() 
+    });
 });
