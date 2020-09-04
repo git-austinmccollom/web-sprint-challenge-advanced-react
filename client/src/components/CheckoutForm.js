c48aef2ec93e6af5a779111f19f5f7db589a111a
git-austinmccollom <austinmccollom@gmail.com>
2020-09-04 10:46:23 -0500

    refactored form state into useForm hook

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
-
-const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
+import useForm from '../hooks/useForm'
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
@@ -15,11 +7,7 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const [ values, setValues, handleChanges ] = useForm();
 
   const handleSubmit = (e) => {
     e.preventDefault();
